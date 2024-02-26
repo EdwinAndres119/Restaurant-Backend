88724632806db7e415b7bb46a6ee631430193807
EdwinAndres119 <125221072+EdwinAndres119@users.noreply.github.com>
2024-02-26 11:41:57 -0500

    application running and database synchronized

---
+++ src/server.js
@@ -0,0 +1,20 @@
+require('./DataBase/sync.js');
+
+const express = require('express');
+const connection = require('./DataBase/connection.js');
+const app = express();
+const port = process.env.PORT|| 1337;
+ 
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+connection.sync({force : false})
+.then(()=> {
+    console.log('Base de datos sincronizada');
+    app.listen(port, ()=>{
+        console.log("la aplicacion corre en el puerto :"+ port);
+    })
+})
+.catch((error) =>{
+    console.error('Error al sincronizar la base de datos:', error);
+});
